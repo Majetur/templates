106a29bc4148e9b324b63d8b1ec3d5ace7492e28
raultm <raultm@gmail.com>
2024-05-16 21:55:59 +0200

    React-starter - wrap fetchApi with default options to test

--- react-starter/src/api/fetchApi.js
+++ react-starter/src/api/fetchApi.js
@@ -1,60 +1,57 @@
+export const fetchApi = async (endpoint, options = {}) => {
+    var defaults = {
+        method: 'GET',
+        headers: {},
+        hasAttachments: false,
+        body: undefined,
+        onUnauthorized: () => "Unauthorized",
+        getAuthToken: () => "",
+        getUsuarioDev: () => "",
+        getPerfilDev: () => "",
+        isPreOrPro: () => true,
+    };
+    let config = Object.assign({}, defaults, options);
 
-//export const fetchApi = async (endpoint, metodo, cuerpo, cabeceraAdicional, tieneAdjuntos = false) => {
-    export const fetchApi = async (endpoint, options = {}) => {
-        var defaults = {
-            method: 'GET',
-            headers: {},
-            hasAttachments: false,
-            body: undefined,
-            onUnauthorized: () => "Unauthorized",
-            getAuthToken: () => "",
-            getUsuarioDev: () => "",
-            getPerfilDev: () => "",
-            isPreOrPro: () => true,
-        };
-        let config = Object.assign({}, defaults, options);
-    
-        let sso = config.getAuthToken();
-        
-        const devHeaders = {}
+    let sso = config.getAuthToken();
 
-        if(!config.isPreOrPro()){
-            let user = config.getUsuarioDev()
-            let profile = config.getPerfilDev()
+    const devHeaders = {}
 
-            if(user) devHeaders["Jwt-User"] = user
-            if(profile) devHeaders["profile"] = profile
+    if (!config.isPreOrPro()) {
+        let user = config.getUsuarioDev()
+        let profile = config.getPerfilDev()
 
-        }
+        if (user) devHeaders["Jwt-User"] = user
+        if (profile) devHeaders["profile"] = profile
 
-        const headers = {
-            ...(config.hasAttachments      ? {} : { 'Content-Type' : 'application/json' }),
-            ...(sso == null || sso == ""   ? {} : { 'Authorization': sso } ),
-            ...config.headers,
-            ...devHeaders
-        }
-        
-        const respuesta = await fetch(endpoint, {
-            method: config.method,
-            headers: headers,
-            body: config.body
-        })
-    
-        switch (respuesta.status) {
-            case 200:
-            case 201:
-            case 202:
-            case 204:
-                return respuesta
-            case 400:
-            case 404:
-                throw await respuesta.json()
-            case 401:
-                config.onUnauthorized()
-                return
-            default:
-                throw respuesta
-        }
-    
     }
-    
+
+    const headers = {
+        ...(config.hasAttachments ? {} : { 'Content-Type': 'application/json' }),
+        ...(sso == null || sso == "" ? {} : { 'Authorization': sso }),
+        ...config.headers,
+        ...devHeaders
+    }
+
+    const respuesta = await fetch(endpoint, {
+        method: config.method,
+        headers: headers,
+        body: config.body
+    })
+
+    switch (respuesta.status) {
+        case 200:
+        case 201:
+        case 202:
+        case 204:
+            return respuesta
+        case 400:
+        case 404:
+            throw await respuesta.json()
+        case 401:
+            config.onUnauthorized()
+            return
+        default:
+            throw respuesta
+    }
+
+}
--- react-starter/tests/api/fetchApi.spec.js
+++ react-starter/tests/api/fetchApi.spec.js
@@ -1,5 +1,5 @@
 import { describe, expect, it, vi, beforeEach } from "vitest";
-import { fetchApi } from "../../src/api/fetchApi";
+import { fetchApi as fetchApiOriginal } from "../../src/api/fetchApi";
 
 function createFetchResponse(status, data) {
     return Promise.resolve({
@@ -8,21 +8,36 @@ function createFetchResponse(status, data) {
     });
 }
 
+const fetchApi = async (endpoint, options) => {
+    var defaults = {
+        method: 'GET',
+        headers: {},
+        hasAttachments: false,
+        body: undefined,
+        onUnauthorized: () => "Unauthorized",
+        getAuthToken: () => "",
+        getUsuarioDev: () => "",
+        getPerfilDev: () => "",
+        isPreOrPro: () => true,
+    };
+    let config = Object.assign({}, defaults, options);
+    return await fetchApiOriginal(endpoint, config)
+}
+
 const fakeendpoint = "/endpoint" //"https//fakeendpoint.com"
 
 describe('fetchApi', () => {
 
     beforeEach(() => {
-        vi.spyOn(global, "fetch").mockImplementationOnce(() => createFetchResponse(200)) 
+        vi.spyOn(global, "fetch").mockImplementationOnce(() => createFetchResponse(200))
     })
 
     it('debe ser una función', () => {
         expect(typeof fetchApi).toBe('function');
     });
 
-
     it('llama a fetch con la url pasada', async () => {
-        
+
         await fetchApi(fakeendpoint)
 
         expect(fetch).toHaveBeenCalledWith(expect.stringContaining(fakeendpoint), expect.anything())
@@ -31,7 +46,7 @@ describe('fetchApi', () => {
     // MARK: Status
     describe("estados", () => {
         it('puede devolver una respuesta 200 - Success', async () => {
-            
+
             expect((await fetchApi(fakeendpoint)).status).toBe(200)
         });
 
@@ -78,35 +93,35 @@ describe('fetchApi', () => {
     // MARK: Methods
     describe("metodos", () => {
         it('se puede usar el metodo GET', async () => {
-            
+
             await fetchApi(fakeendpoint, { method: 'GET' })
 
             expect(fetch).toHaveBeenCalledWith(expect.stringContaining(fakeendpoint), expect.objectContaining({ method: 'GET' }))
         })
 
         it('por defecto se usa el metodo GET', async () => {
-            
+
             await fetchApi(fakeendpoint)
 
             expect(fetch).toHaveBeenCalledWith(expect.stringContaining(fakeendpoint), expect.objectContaining({ method: 'GET' }))
         })
 
         it('se puede usar el metodo POST', async () => {
-            
+
             await fetchApi(fakeendpoint, { method: 'POST' })
 
             expect(fetch).toHaveBeenCalledWith(expect.stringContaining(fakeendpoint), expect.objectContaining({ method: 'POST' }))
         })
 
         it('se puede usar el metodo PUT', async () => {
-            
+
             await fetchApi(fakeendpoint, { method: 'PUT' })
 
             expect(fetch).toHaveBeenCalledWith(expect.stringContaining(fakeendpoint), expect.objectContaining({ method: 'PUT' }))
         })
 
         it('se puede usar el metodo DELETE', async () => {
-            
+
             await fetchApi(fakeendpoint, { method: 'DELETE' })
 
             expect(fetch).toHaveBeenCalledWith(expect.stringContaining(fakeendpoint), expect.objectContaining({ method: 'DELETE' }))
@@ -116,21 +131,21 @@ describe('fetchApi', () => {
     // MARK: Headers
     describe("cabeceras", () => {
         it('por defecto el Content-Type es application/json', async () => {
-            
+
             await fetchApi(fakeendpoint)
 
             expect(fetch).toHaveBeenCalledWith(expect.stringContaining(fakeendpoint), expect.objectContaining({ headers: { "Content-Type": "application/json" } }))
         })
 
         it('si metodo va marcado con adjuntos quita el Content-Type', async () => {
-            
+
             await fetchApi(fakeendpoint, { hasAttachments: true })
 
             expect(fetch).toHaveBeenCalledWith(expect.stringContaining(fakeendpoint), expect.not.objectContaining({ headers: { "Content-Type": "application/json" } }))
         })
 
         it('si puede obtener un SSO lo añade a la cabecera', async () => {
-            
+
             let token = "Bearer tokendeprueba"
 
             await fetchApi(fakeendpoint, { getAuthToken: () => token })
@@ -139,28 +154,28 @@ describe('fetchApi', () => {
         })
 
         it('se puede añadir una cabecera adicional', async () => {
-            
+
             await fetchApi(fakeendpoint, { headers: { "test": "test" } })
 
             expect(fetch).toHaveBeenCalledWith(expect.stringContaining(fakeendpoint), expect.objectContaining({ headers: expect.objectContaining({ "test": "test" }) }))
         })
 
         it('se pueden añadir varias cabeceras adicional', async () => {
-            
+
             await fetchApi(fakeendpoint, { headers: { "test1": "test1", "test2": "test2", "test3": "test3" } })
 
             expect(fetch).toHaveBeenCalledWith(expect.stringContaining(fakeendpoint), expect.objectContaining({ headers: expect.objectContaining({ "test1": "test1", "test2": "test2", "test3": "test3" }) }))
         })
 
         it('si no estoy en entornos pre/pro me deja añadir cabecera de usuario', async () => {
-            
+
             await fetchApi(fakeendpoint, { isPreOrPro: () => false, getUsuarioDev: () => "test.demo" })
 
             expect(fetch).toHaveBeenCalledWith(expect.stringContaining(fakeendpoint), expect.objectContaining({ headers: expect.objectContaining({ "Jwt-User": "test.demo" }) }))
         })
 
         it('si no estoy en entornos pre/pro me deja añadir cabecera de perfil', async () => {
-            
+
             await fetchApi(fakeendpoint, { isPreOrPro: () => false, getPerfilDev: () => "developer" })
 
             expect(fetch).toHaveBeenCalledWith(expect.stringContaining(fakeendpoint), expect.objectContaining({ headers: expect.objectContaining({ "profile": "developer" }) }))
@@ -181,12 +196,18 @@ describe('fetchApi', () => {
         it('se puede pasar un body', async () => {
             vi.spyOn(global, "fetch").mockImplementationOnce(() => createFetchResponse(200))
 
-            await fetchApi(fakeendpoint, { body: {"test": "test"}})
+            await fetchApi(fakeendpoint, { body: { "test": "test" } })
 
-            expect(fetch).toHaveBeenCalledWith(expect.stringContaining(fakeendpoint), expect.objectContaining({ body: {"test": "test"} }))
+            expect(fetch).toHaveBeenCalledWith(expect.stringContaining(fakeendpoint), expect.objectContaining({ body: { "test": "test" } }))
         })
     })
 
+    // MARK: sso
+    // https://stackoverflow.com/questions/59417195/how-do-i-mock-a-return-value-multiple-times-with-different-values-in-the-same-te
+    describe('sso - gestion de token y reintentos', () => {
+        it('si está en modo desarrollo no comprueba token/expiracion/loquesea', () => {
 
+        })
+    })
 
 });
